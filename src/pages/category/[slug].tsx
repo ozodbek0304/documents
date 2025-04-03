403b90666ff565d0237627abb81804827b09f286
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-04-03 15:18:57 +0500

    ISR requests

--- src/pages/category/[slug].tsx
+++ src/pages/category/[slug].tsx
@@ -1,30 +1,68 @@
 import Layout from "@/components/layout";
 import React from "react";
-import { useGet } from "@/hooks/useGet";
+import { getRequest } from "@/hooks/useGet";
 import { CATEGORIES, PRODUCTS_HOME } from "@/lib/api-endpoints";
 import CategorySidebar from "@/components/pages/category";
-import { useRouter } from "next/router";
 import ProductCard from "@/components/shared/product-card";
 import ParamInput from "@/components/param/input";
 import { Document } from "@/types/products";
+import { CategoriesType } from "@/views/home/hero";
+import { toast } from "sonner";
 
-const CategoryPage = () => {
-  const { data } = useGet(CATEGORIES);
-  const { query } = useRouter();
-  const currentSlug = Array.isArray(query.slug)
-    ? query.slug[0]
-    : query.slug || "";
-  const {
-    data: document,
-    isSuccess,
-  } = useGet<{
+interface CategoryPageProps {
+  categories: CategoriesType[];
+  document: {
     next_cursor: number;
     products: Document[];
-  }>(`${PRODUCTS_HOME}/${currentSlug}`, {
-    params: { search: query.search },
-    options: { enabled: Boolean(currentSlug) || Boolean(query.search !== "") },
-  });
+  };
+  currentSlug: string;
+}
 
+export async function getStaticPaths() {
+  const categories = await getRequest(CATEGORIES);
+
+  const paths = categories.map((category: { slug: string }) => ({
+    params: { slug: category.slug },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  let categories = [];
+  let document = null;
+
+  try {
+    categories = await getRequest(CATEGORIES);
+  } catch (error: any) {
+    toast.error(error);
+    categories = [];
+  }
+  try {
+    document = await getRequest(`${PRODUCTS_HOME}/${slug}`);
+  } catch (error: any) {
+    toast.error(error);
+    categories = [];
+  }
+
+  return {
+    props: {
+      document,
+      categories,
+    },
+    revalidate: 10,
+  };
+}
+
+const CategoryPage = ({
+  currentSlug,
+  categories,
+  document,
+}: CategoryPageProps) => {
   return (
     <Layout>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-6 py-8 sm:px-0 px-3">
@@ -38,14 +76,12 @@ const CategoryPage = () => {
           />
         </div>
         <div className="col-span-1 hidden lg:block">
-          <CategorySidebar categories={data || []} currentSlug={currentSlug} />
+          <CategorySidebar categories={categories} currentSlug={currentSlug} />
         </div>
         <div className="lg:col-span-3 col-span-4 items-start grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 py-2 gap-3">
-          {isSuccess &&
-            document.products.length > 0 &&
-            document.products.map((item, index) => (
-              <ProductCard key={index} product={item} />
-            ))}
+          {document.products.map((item, index) => (
+            <ProductCard key={index} product={item} />
+          ))}
         </div>
       </div>
     </Layout>
