1e9aaeca13d418b4b45b040eee30ef34c5399233
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-04-07 15:27:12 +0500

    change dojen

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["d2gaf67z4e11li.cloudfront.net"],
+    domains: [
+      "d2gaf67z4e11li.cloudfront.net",
+      "d280q91s0zg1ey.cloudfront.net",
+    ],
   },
 };
 
