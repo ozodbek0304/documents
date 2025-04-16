08130c42328cf0776082179556bb336d81d73e7c
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-04-16 22:53:32 +0500

    aa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,27 +3,19 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
-import { getRequest } from "@/hooks/useGet";
+import { useGet } from "@/hooks/useGet";
 import { generateAuthKey } from "@/components/header";
 import { GET_VIEW } from "@/lib/api-endpoints";
 
-export async function getServerSideProps() {
-  try {
-    await getRequest(GET_VIEW, {
+export default function App({ Component, pageProps }: AppProps) {
+  const {} = useGet(GET_VIEW, {
+    config: {
       headers: {
         Auth: generateAuthKey(),
       },
-    });
-  } catch (error: any) {
-    console.error("Error fetching data:", error);
-  }
+    },
+  });
 
-  return {
-    props: {}, // so'rov ma'lumotlarini props sifatida qaytarib yuborish
-  };
-}
-
-export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Providers>
