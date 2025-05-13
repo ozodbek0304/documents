6d352590ae5f5a68e1264d8ed54270d39738d613
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-05-13 11:33:36 +0500

    login

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,37 +1,45 @@
+import { useAuthStore } from "@/store/auth-store";
 import axios from "axios";
+export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-
-export const api = axios.create({
-  baseURL: baseURL,
-  timeout: 30000,
-  withCredentials: true,
+export const axiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 30000,
 });
 
 export const getToken = () => {
-  if (typeof window !== "undefined") {
-    return localStorage.getItem("token");
-  }
-  return null;
+    if (typeof window !== "undefined") {
+        return localStorage.getItem("token");
+    }
+    return null;
 };
 
-api.interceptors.request.use(
-  (config) => {
-    const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = getToken();
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    return Promise.reject(error);
-  }
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const status = error?.response?.status;
+
+        if (status === 401 || status === 403) {
+            if (typeof window !== "undefined") {
+                const { clearToken } = useAuthStore.getState();
+                clearToken();
+            }
+        }
+
+        return Promise.reject(error);
+    }
 );
