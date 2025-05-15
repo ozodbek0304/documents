5cf368ef40e6bdeb56fdaa6d007b87740e248726
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-05-15 15:53:25 +0500

    sz

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -6,6 +6,7 @@ export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 export const api = axios.create({
     baseURL: baseURL,
     timeout: 30000,
+    withCredentials: true
 });
 
 export const getToken = () => {
